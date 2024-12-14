29f420ac8dedfbbb1ee610ce5ae9b8374669e13d
autstrp <45160755+autstrp@users.noreply.github.com>
2024-12-14 22:28:10 +0900

    feat: remove unnecessary pre-installed apps

---
+++ run_once_remove_preinstalled_apps.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+set -euxo pipefail
+
+# http://slapper.sblo.jp/article/189636531.html
+
+type libreoffice >/dev/null 2>&1 && sudo apt-get remove --purge libreoffice*
+type thunderbird >/dev/null 2>&1 && sudo apt-get remove --purge thunderbird*
+sudo apt-get clean
+sudo apt-get autoremove
