ec9a82bcc73fa8c8a8c8c46c1a5e7a2f4bc65df5
autstrp <45160755+autstrp@users.noreply.github.com>
2024-12-15 03:04:15 +0900

    fix: allow apt-get to work non-interatively

--- .chezmoiscripts/run_once_after_99_remove_preinstalled_apps.sh
+++ .chezmoiscripts/run_once_after_99_remove_preinstalled_apps.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 # http://slapper.sblo.jp/article/189636531.html
 
-type libreoffice >/dev/null 2>&1 && sudo apt-get remove --purge "libreoffice*"
-type thunderbird >/dev/null 2>&1 && sudo apt-get remove --purge "thunderbird*"
-sudo apt-get clean
-sudo apt-get autoremove
+type libreoffice >/dev/null 2>&1 && sudo apt-get remove --purge "libreoffice*" -y
+type thunderbird >/dev/null 2>&1 && sudo apt-get remove --purge "thunderbird*" -y
+sudo apt-get clean -y
+sudo apt-get autoremove -y
