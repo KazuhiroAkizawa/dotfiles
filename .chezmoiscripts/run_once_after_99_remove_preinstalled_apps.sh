5c99bb3e3ecac35cf5f0af0cfa34ef40f4aa8233
autstrp <45160755+autstrp@users.noreply.github.com>
2024-12-15 02:42:02 +0900

    fix: add asterisk quotation for zsh

--- .chezmoiscripts/run_once_after_99_remove_preinstalled_apps.sh
+++ .chezmoiscripts/run_once_after_99_remove_preinstalled_apps.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 # http://slapper.sblo.jp/article/189636531.html
 
-type libreoffice >/dev/null 2>&1 && sudo apt-get remove --purge libreoffice*
-type thunderbird >/dev/null 2>&1 && sudo apt-get remove --purge thunderbird*
+type libreoffice >/dev/null 2>&1 && sudo apt-get remove --purge "libreoffice*"
+type thunderbird >/dev/null 2>&1 && sudo apt-get remove --purge "thunderbird*"
 sudo apt-get clean
 sudo apt-get autoremove
